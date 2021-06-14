7bb799eef2977133af88a3c6bb5a90447c69b968
Mihail <mwxood@gmail.com>
2021-06-14 19:37:42 +0300

    init

--- contact.php
+++ contact.php
@@ -15,145 +15,230 @@
     </div>
     <!--end search-form-holder-->
     <section class="inner-section contact-section">
-            
+
 
         <div class="contact">
-        <div class="row">
-            <div class="col-md-6">
-                <div class="map-holder">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2934.133460928514!2d23.31202841569965!3d42.65852712419249!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40aa845e8f48868d%3A0x4bb1ba39d0b5e7d!2sul.%20%E2%80%9EHenrik%20Ibsen%E2%80%9C%205%2C%201407%20Hladilnika%2C%20Sofia!5e0!3m2!1sen!2sbg!4v1623683183065!5m2!1sen!2sbg"  style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-                </div><!--end map-holder-->
-            </div>
-
-            <div class="col-md-6">
-                <div class="contacts-title">
-                <ul class="breadcrumbs">
-                        <li><a href="/">Начало</a></li>
-                        <li>Контакти</li>
-                    </ul>
-                    <h1>Сравнение на продуктите</h1> 
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="map-holder">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2934.133460928514!2d23.31202841569965!3d42.65852712419249!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40aa845e8f48868d%3A0x4bb1ba39d0b5e7d!2sul.%20%E2%80%9EHenrik%20Ibsen%E2%80%9C%205%2C%201407%20Hladilnika%2C%20Sofia!5e0!3m2!1sen!2sbg!4v1623683183065!5m2!1sen!2sbg" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                    </div>
+                    <!--end map-holder-->
                 </div>
-                <div class="form-holder">
-                   
 
-                    <span class="contact-title">Попълнете формата, за да се свържете с нас</span>
+                <div class="col-md-6">
+                    <div class="contacts-title">
+                        <ul class="breadcrumbs">
+                            <li><a href="/">Начало</a></li>
+                            <li>Контакти</li>
+                        </ul>
+                        <h1>Сравнение на продуктите</h1>
+                    </div>
+                    <div class="form-holder">
 
-                    <form class="contact-form" method="post" action="">
-                        <div class="form-group">
-                            <label>
-                                <input type="text" placeholder="Име" class="input-bg" name="name" >
-                            </label>
 
-                            <label>
-                                <input type="text" placeholder="Фамилия" class="input-bg" name="last_name" >
-                            </label>
+                        <span class="contact-title">Попълнете формата, за да се свържете с нас</span>
 
-                            <label>
-                                <input type="wmail" placeholder="Емаил" class="input-bg" name="емаил" >
-                            </label>
-                        </div>
+                        <form class="contact-form" method="post" action="">
+                            <div class="form-group">
+                                <label>
+                                    <input type="text" placeholder="Име" class="input-bg" name="name">
+                                </label>
 
-                        <div class="form-group">
-                            <label>
-                                <input type="text" placeholder="Телефон" class="input-bg" name="phone" >
-                            </label>
+                                <label>
+                                    <input type="text" placeholder="Фамилия" class="input-bg" name="last_name">
+                                </label>
 
-                            <label>
-                                <input type="text" placeholder="Компания" class="input-bg" name="company" >
-                            </label>
+                                <label>
+                                    <input type="wmail" placeholder="Емаил" class="input-bg" name="емаил">
+                                </label>
+                            </div>
 
-                          
+                            <div class="form-group">
+                                <label>
+                                    <input type="text" placeholder="Телефон" class="input-bg" name="phone">
+                                </label>
+
+                                <label>
+                                    <input type="text" placeholder="Компания" class="input-bg" name="company">
+                                </label>
+
+
                                 <div class="custom-select filter-select">
                                     <label>
-                                    <select name="name" class="filter" id="brand">
-                                        <option selected value="До администрация">До администрация</option>
-                                        <option value="До администрация">До администрация</option>
-                                    </select>
+                                        <select name="name" class="filter" id="brand">
+                                            <option selected value="До администрация">До администрация</option>
+                                            <option value="До администрация">До администрация</option>
+                                        </select>
                                     </label>
+                                </div>
+                                <!--end custom-select-->
+
+
+
+
                             </div>
-                            <!--end custom-select-->
 
+                            <textarea class="input-bg message" name="message" placeholder="Вашето съобщение"></textarea>
 
-                           
-                          
-                        </div>
-
-                        <textarea class="input-bg message" name="message" placeholder="Вашето съобщение" ></textarea>
-
-                        <div class="captcha-holder">
-                            <span class="captcha"></span>
-                            <input type="submit" class="input-submit" value="Изпрати" >
-                        </div>
-                    </form>
-                </div><!--end map-holder-->
+                            <div class="captcha-holder">
+                                <span class="captcha"></span>
+                                <input type="submit" class="input-submit" value="Изпрати">
+                            </div>
+                        </form>
+                    </div>
+                    <!--end map-holder-->
+                </div>
             </div>
         </div>
-        </div><!--end contact-->
+        <!--end contact-->
 
         <div class="contact-address">
-                <div class="custom-container">
-                    <div class="row">
-                        <div class="col-sm-2 col-md-3">
-                            <span class="person-name">
-                                Георги Тодоров
-                                <span class="person-position">/Супервайзор/</span>
-                            </span>
-                            <span class="person-contact">
-                           <span> Източна България</span>
+            <div class="custom-container">
+                <div class="row">
+                    <div class="col-sm-6 col-md-3">
+                        <span class="person-name">
+                            Георги Тодоров
+                            <span class="person-position">/Супервайзор/</span>
+                        </span>
+                        <span class="person-contact">
+                            <span> Източна България</span>
                             <a href="tel: 0882240948">GSM: 0882 240 948</a>
                             <a href="mailto: g.todorov@euromasterbg.com">g.todorov@euromasterbg.com</a>
-                            </span>
-                        </div>
-
-                        <div class="col-sm-2 col-md-3">
-                            <span class="person-name">
-                            Диана Замфирова
-                                <span class="person-position">/Онлайн търговия и фирмени магазини/</span>
-                            </span>
-                            <span class="person-contact">
-                           <span> Източна България</span>
-                            <a href="tel: 0884605647">GSM: 0884 605 647</a>
-                            <a href="mailto: g.todorov@euromasterbg.com">g.todorov@euromasterbg.com</a>
-                            </span>
-                        </div>
-
-                        <div class="col-sm-2 col-md-3">
-                            <span class="person-name">
-                                Георги Тодоров
-                                <span class="person-position">/Супервайзор/</span>
-                            </span>
-                            <span class="person-contact">
-                           <span> Източна България</span>
-                            <a href="tel: 0882240948">GSM: 0882 240 948</a>
-                            <a href="mailto: g.todorov@euromasterbg.com">g.todorov@euromasterbg.com</a>
-                            </span>
-                        </div>
-
-                        
-
-                        <div class="col-sm-2 col-md-3">
-                            <span class="person-name">
-                            Диана Замфирова
-                                <span class="person-position">/Онлайн търговия и фирмени магазини/</span>
-                            </span>
-                            <span class="person-contact">
-                           <span> Източна България</span>
-                            <a href="tel: 0884605647">GSM: 0884 605 647</a>
-                            <a href="mailto: g.todorov@euromasterbg.com">g.todorov@euromasterbg.com</a>
-                            </span>
-                        </div>
+                        </span>
                     </div>
 
-                    <div class="object-addres">
-                        <div class="address-custom-row">
-                            <div class="address-col">
-dasda
-                            </div><!--end address-col-->
-                        </div><!--end row--->
-                    </div><!--end obj-address-->
+                    <div class="col-sm-6 col-md-3">
+                        <span class="person-name">
+                            Диана Замфирова
+                            <span class="person-position">/Онлайн търговия и фирмени магазини/</span>
+                        </span>
+                        <span class="person-contact">
+                            <span> Източна България</span>
+                            <a href="tel: 0884605647">GSM: 0884 605 647</a>
+                            <a href="mailto: g.todorov@euromasterbg.com">g.todorov@euromasterbg.com</a>
+                        </span>
+                    </div>
+
+                    <div class="col-sm-6 col-md-3">
+                        <span class="person-name">
+                            Георги Тодоров
+                            <span class="person-position">/Супервайзор/</span>
+                        </span>
+                        <span class="person-contact">
+                            <span> Източна България</span>
+                            <a href="tel: 0882240948">GSM: 0882 240 948</a>
+                            <a href="mailto: g.todorov@euromasterbg.com">g.todorov@euromasterbg.com</a>
+                        </span>
+                    </div>
+
+
+
+                    <div class="col-sm-6 col-md-3">
+                        <span class="person-name">
+                            Диана Замфирова
+                            <span class="person-position">/Онлайн търговия и фирмени магазини/</span>
+                        </span>
+                        <span class="person-contact">
+                            <span> Източна България</span>
+                            <a href="tel: 0884605647">GSM: 0884 605 647</a>
+                            <a href="mailto: g.todorov@euromasterbg.com">g.todorov@euromasterbg.com</a>
+                        </span>
+                    </div>
                 </div>
+
+
+            </div>
         </div>
-            
+
+        <div class="object-addres">
+            <div class="custom-container">
+                <div class="address-custom-row">
+                    <div class="address-col">
+                        <span class="address-title">
+                            София
+                            <span class="address-desc">Централен офис и магазин</span>
+                        </span>
+                        <span class="address">
+                            <span>Бул. “Ломско шосе”№ 246</span>
+                            <span>Тел: <a href="tel:070044155">0700 44 155</a> за цялата страна</span>
+                            <span>Факс: 02/934 33 33</span>
+                            <span><a href="mailto: info@euromasterbg.com">info@euromasterbg.com</a></span>
+                            <span><a href="mailto: sales_department@euromasterbg.com">sales_department@euromasterbg.com</a></span>
+                        </span>
+                        <a href="#" class="show-on-map">Покажи на карта</a>
+                    </div>
+                    <!--end address-col-->
+
+                    <div class="address-col">
+                        <span class="address-title">
+                            София
+                            <span class="address-desc">Централен офис и магазин</span>
+                        </span>
+                        <span class="address">
+                            <span>Бул. “Ломско шосе”№ 246</span>
+                            <span>Тел: <a href="tel:070044155">0700 44 155</a> за цялата страна</span>
+                            <span>Факс: 02/934 33 33</span>
+                            <span><a href="mailto: info@euromasterbg.com">info@euromasterbg.com</a></span>
+                            <span><a href="mailto: sales_department@euromasterbg.com">sales_department@euromasterbg.com</a></span>
+                        </span>
+                        <a href="#" class="show-on-map">Покажи на карта</a>
+                    </div>
+                    <!--end address-col-->
+
+                    <div class="address-col">
+                        <span class="address-title">
+                            Пловдив
+                            <span class="address-desc">Централен офис и магазин</span>
+                        </span>
+                        <span class="address">
+                            <span>Бул. “Ломско шосе”№ 246</span>
+                            <span>Тел: <a href="tel:070044155">0700 44 155</a> за цялата страна</span>
+                            <span>Факс: 02/934 33 33</span>
+                            <span><a href="mailto: info@euromasterbg.com">info@euromasterbg.com</a></span>
+                            <span><a href="mailto: sales_department@euromasterbg.com">sales_department@euromasterbg.com</a></span>
+                        </span>
+                        <a href="#" class="show-on-map">Покажи на карта</a>
+                    </div>
+                    <!--end address-col-->
+
+                    <div class="address-col">
+                        <span class="address-title">
+                            Влико Търново
+                            <span class="address-desc">Централен офис и магазин</span>
+                        </span>
+                        <span class="address">
+                            <span>Бул. “Ломско шосе”№ 246</span>
+                            <span>Тел: <a href="tel:070044155">0700 44 155</a> за цялата страна</span>
+                            <span>Факс: 02/934 33 33</span>
+                            <span><a href="mailto: info@euromasterbg.com">info@euromasterbg.com</a></span>
+                            <span><a href="mailto: sales_department@euromasterbg.com">sales_department@euromasterbg.com</a></span>
+                        </span>
+                        <a href="#" class="show-on-map">Покажи на карта</a>
+                    </div>
+                    <!--end address-col-->
+
+                    <div class="address-col">
+                        <span class="address-title">
+                            Румъния
+                            <span class="address-desc">Централен офис и магазин</span>
+                        </span>
+                        <span class="address">
+                            <span>Бул. “Ломско шосе”№ 246</span>
+                            <span>Тел: <a href="tel:070044155">0700 44 155</a> за цялата страна</span>
+                            <span>Факс: 02/934 33 33</span>
+                            <span><a href="mailto: info@euromasterbg.com">info@euromasterbg.com</a></span>
+                            <span><a href="mailto: sales_department@euromasterbg.com">sales_department@euromasterbg.com</a></span>
+                        </span>
+                        <a href="#" class="show-on-map">Покажи на карта</a>
+                    </div>
+                    <!--end address-col-->
+                </div>
+                <!--end row--->
+            </div>
+        </div>
+        <!--end obj-address-->
+
     </section>
 </main>
 <?php include('includes/footer.php') ?>
